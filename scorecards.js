ab0eaa060d2edda6ca72403b5ed83f82d0279005
treyturley <81778542+treyturley@users.noreply.github.com>
2022-09-14 22:31:19 -0500

    comments out testing scorecards

--- scorecards.js
+++ scorecards.js
@@ -3,120 +3,120 @@
 
 // pre-defined scorecards array for testing
 const scorecards = [
-  {
-    id: "abcd",
-    name: "test",
-    gameStatus: "STARTED",
-    scorecard: [
-      {
-        "playerName": "Tim",
-        "roundNumber": 1,
-        "bid": 1,
-        "tricks": 1,
-        "bonus": 0,
-        "roundTotal": 20
-      },
-      {
-        "playerName": "Bob",
-        "roundNumber": 1,
-        "bid": 1,
-        "tricks": 0,
-        "bonus": 0,
-        "roundTotal": -10
-      },
-      {
-        "playerName": "Foo",
-        "roundNumber": 1,
-        "bid": 0,
-        "tricks": 0,
-        "bonus": 0,
-        "roundTotal": 10
-      },
-      {
-        "playerName": "Bar",
-        "roundNumber": 1,
-        "bid": 0,
-        "tricks": 0,
-        "bonus": 10,
-        "roundTotal": 20
-      }
-    ],
-    playerTotals: [
-      {
-        "playerName": "Tim",
-        "total": 20,
-      },
-      {
-        "playerName": "Bob",
-        "total": -10,
-      },
-      {
-        "playerName": "Foo",
-        "total": 10,
-      },
-      {
-        "playerName": "Bar",
-        "total": 20,
-      }
-    ]
-  }, 
-  {
-    id: "deleteme",
-    name: "test_delete",
-    gameStatus: "FINISHED",
-    scorecard: [
-      {
-        "playerName": "Tim",
-        "roundNumber": 1,
-        "bid": 1,
-        "tricks": 1,
-        "bonus": 0,
-        "roundTotal": 20
-      },
-      {
-        "playerName": "Bob",
-        "roundNumber": 1,
-        "bid": 1,
-        "tricks": 0,
-        "bonus": 0,
-        "roundTotal": -10
-      },
-      {
-        "playerName": "foo",
-        "roundNumber": 1,
-        "bid": 0,
-        "tricks": 0,
-        "bonus": 0,
-        "roundTotal": 10
-      },
-      {
-        "playerName": "bar",
-        "roundNumber": 1,
-        "bid": 0,
-        "tricks": 0,
-        "bonus": 10,
-        "roundTotal": 20
-      }
-    ],
-    playerTotals: [
-      {
-        "playerName": "Tim",
-        "total": 20,
-      },
-      {
-        "playerName": "Bob",
-        "total": -10,
-      },
-      {
-        "playerName": "Foo",
-        "total": 10,
-      },
-      {
-        "playerName": "Bar",
-        "total": 20,
-      }
-    ]
-  }
+  // {
+  //   id: "abcd",
+  //   name: "test",
+  //   status: "STARTED",
+  //   scorecard: [
+  //     {
+  //       "playerName": "Tim",
+  //       "roundNumber": 1,
+  //       "bid": 1,
+  //       "tricks": 1,
+  //       "bonus": 0,
+  //       "roundTotal": 20
+  //     },
+  //     {
+  //       "playerName": "Bob",
+  //       "roundNumber": 1,
+  //       "bid": 1,
+  //       "tricks": 0,
+  //       "bonus": 0,
+  //       "roundTotal": -10
+  //     },
+  //     {
+  //       "playerName": "Foo",
+  //       "roundNumber": 1,
+  //       "bid": 0,
+  //       "tricks": 0,
+  //       "bonus": 0,
+  //       "roundTotal": 10
+  //     },
+  //     {
+  //       "playerName": "Bar",
+  //       "roundNumber": 1,
+  //       "bid": 0,
+  //       "tricks": 0,
+  //       "bonus": 10,
+  //       "roundTotal": 20
+  //     }
+  //   ],
+  //   playerTotals: [
+  //     {
+  //       "playerName": "Tim",
+  //       "total": 20,
+  //     },
+  //     {
+  //       "playerName": "Bob",
+  //       "total": -10,
+  //     },
+  //     {
+  //       "playerName": "Foo",
+  //       "total": 10,
+  //     },
+  //     {
+  //       "playerName": "Bar",
+  //       "total": 20,
+  //     }
+  //   ]
+  // }, 
+  // {
+  //   id: "deleteme",
+  //   name: "test_delete",
+  //   status: "FINISHED",
+  //   scorecard: [
+  //     {
+  //       "playerName": "Tim",
+  //       "roundNumber": 1,
+  //       "bid": 1,
+  //       "tricks": 1,
+  //       "bonus": 0,
+  //       "roundTotal": 20
+  //     },
+  //     {
+  //       "playerName": "Bob",
+  //       "roundNumber": 1,
+  //       "bid": 1,
+  //       "tricks": 0,
+  //       "bonus": 0,
+  //       "roundTotal": -10
+  //     },
+  //     {
+  //       "playerName": "foo",
+  //       "roundNumber": 1,
+  //       "bid": 0,
+  //       "tricks": 0,
+  //       "bonus": 0,
+  //       "roundTotal": 10
+  //     },
+  //     {
+  //       "playerName": "bar",
+  //       "roundNumber": 1,
+  //       "bid": 0,
+  //       "tricks": 0,
+  //       "bonus": 10,
+  //       "roundTotal": 20
+  //     }
+  //   ],
+  //   playerTotals: [
+  //     {
+  //       "playerName": "Tim",
+  //       "total": 20,
+  //     },
+  //     {
+  //       "playerName": "Bob",
+  //       "total": -10,
+  //     },
+  //     {
+  //       "playerName": "Foo",
+  //       "total": 10,
+  //     },
+  //     {
+  //       "playerName": "Bar",
+  //       "total": 20,
+  //     }
+  //   ]
+  // }
 ];
 
 
